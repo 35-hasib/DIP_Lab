0f670534949384ccff746dbb3f08b6ea0124b677
35-hasib <hrrahman35@gmail.com>
2024-10-06 11:12:38 +0600

    Update smooth_sharp.m

--- test.m
+++ test.m
@@ -4,9 +4,7 @@ a = imresize(a,[300,300]);
 figure
 imshow(a);
 
-m = [1,1,1; %%For smoth       %% m = [-1,-1,-1; For sharp
-     1,1,1;                   %%      -1,9,-1;
-     1,1,1];                  %%      -1,-1,-1];
+
 
 figure
-imshow(smooth_sharp(a,m));
+imshow(smooth_sharp(a));
